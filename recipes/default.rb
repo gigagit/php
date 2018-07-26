457a21406b3def4b1ff9a07e44f4730cb8fee3fc
Harpreet Singh Rayat <rayat005@hotmail.com>
2018-07-26 18:29:43 +0530

    - initial commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,17 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+app = search(:aws_opsworks_app).first
+app_path = "/var/www/html/#{app['shortname']}"
+
+package "git" do
+  options "--force-yes" 
+end
+
+application app_path do
+  git app_path do
+    repository app["app_source"]["url"]
+    revision app["app_source"]["revision"]
+    action :sync
+  end
+end
