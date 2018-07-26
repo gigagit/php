9c9da8ef86539d7a2e29c541ff68b5ab49146c99
Harpreet Singh Rayat <rayat005@hotmail.com>
2018-07-26 19:17:48 +0530

    - added git client

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,6 +6,10 @@
 app = search(:aws_opsworks_app).first
 app_path = "/var/www/html/#{app['shortname']}"
 
+git_client 'default' do
+    action :install
+end
+
 deploy app_path do
     repo app["app_source"]["url"]
     revision app["app_source"]["revision"]
