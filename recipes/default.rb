2cd27c1d903f44bd44e10d34477075859fc80cc6
Harpreet Singh Rayat <rayat005@hotmail.com>
2018-07-26 18:49:43 +0530

    - removed git

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,10 +6,6 @@
 app = search(:aws_opsworks_app).first
 app_path = "/var/www/html/#{app['shortname']}"
 
-package "git" do
-  options "--force-yes" 
-end
-
 deploy app_path do
     repo app["app_source"]["url"]
     revision app["app_source"]["revision"]
